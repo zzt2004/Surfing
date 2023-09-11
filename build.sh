5c376401f0378a02339261e51c6f05ea0e8647c4
staryuboy@gmail.com <staryuboy@gmail.com>
2023-09-12 03:51:13 +0800

    Chore: Update Build

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 version=$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}')
-filename="Surfing_${version}_release.zip"
+
+if [ "$isAlpha" = true ]; then
+    filename="Surfing_${version}_alpha.zip"
+else
+    filename="Surfing_${version}_release.zip"
+fi
+
 zip -r -o -X -ll "$filename" ./ -x '.git/*' -x '.github/*' -x 'folder/*' -x 'build.sh' -x 'Surfing.json'
