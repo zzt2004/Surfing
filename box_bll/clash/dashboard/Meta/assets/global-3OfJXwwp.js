a64d4c9507460c646484ec235a46bcee9a6bca3f
MoGuangYu <staryuboy@gmail.com>
2024-07-13 18:52:43 +0800

    Update：Meta panel v1.141.1

--- box_bll/clash/dashboard/Meta/assets/global-3OfJXwwp.js
+++
@@ -1,1 +0,0 @@
-import{a5 as a,a_ as m}from"./index-B3l_s7jA.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
---
+++ box_bll/clash/dashboard/Meta/assets/global-hC0rTsfE.js
@@ -0,0 +1,1 @@
+import{a6 as a,b6 as m}from"./index-o5lPLXoZ.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
