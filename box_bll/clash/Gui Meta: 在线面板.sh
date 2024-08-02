e4e6572328d29f66953226c8c791eac5cbb83de3
MoGuangYu <staryuboy@gmail.com>
2024-08-03 02:40:09 +0800

    Delete

--- box_bll/clash/Gui Meta: 在线面板.sh
+++
@@ -1,2 +0,0 @@
-#!/system/bin/sh
-am start -a android.intent.action.VIEW -d "https://metacubex.github.io/metacubexd"
--- box_bll/clash/Gui Yacd: 在线面板.sh
+++
@@ -1,2 +0,0 @@
-#!/system/bin/sh
-am start -a android.intent.action.VIEW -d "https://yacd.metacubex.one/"
