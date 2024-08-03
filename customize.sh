d8c91351d514528beced20e33dc6a4b24533a3f6
MoGuangYu <staryuboy@gmail.com>
2024-08-04 05:01:31 +0800

    Update: Installation

--- customize.sh
+++ customize.sh
@@ -3,44 +3,50 @@
 SKIPUNZIP=1
 ASH_STANDALONE=1
 
-if [ "$BOOTMODE" ! = true ] ; then
+
+if [ "$BOOTMODE" != true ]; then
   abort "Error: 请在 Magisk Manager / KernelSU Manager / APatch 中安装"
-elif [ "$KSU" = true ] && [ "$KSU_VER_CODE" -lt 10670 ] ; then
+elif [ "$KSU" = true ] && [ "$KSU_VER_CODE" -lt 10670 ]; then
   abort "Error: 请更新您的 KernelSU Manager 版本"
 fi
 
-if [ "$KSU" = true ] && [ "$KSU_VER_CODE" -lt 10683 ] ; then
+if [ "$KSU" = true ] && [ "$KSU_VER_CODE" -lt 10683 ]; then
   service_dir="/data/adb/ksu/service.d"
 else 
   service_dir="/data/adb/service.d"
 fi
 
-if [ ! -d "$service_dir" ] ; then
-    mkdir -p $service_dir
+if [ ! -d "$service_dir" ]; then
+  mkdir -p "$service_dir"
 fi
 
-unzip -qo "${ZIPFILE}" -x 'META-INF/*' -d $MODPATH
-if [ -d /data/adb/box_bll ] ; then
-  cp /data/adb/box_bll/clash/config.yaml /data/adb/box_bll/clash/config.yaml.bak
-  cp /data/adb/box_bll/scripts/box.config /data/adb/box_bll/scripts/box.config.bak
-  
-  rm -rf "/data/adb/box_bll/clash/Gui Yacd: 获取面板.sh"
-  rm -rf "/data/adb/box_bll/clash/Gui Meta: 获取面板.sh"
-  rm -rf "/data/adb/box_bll/clash/Telegram chat.sh"
-  rm -rf "/data/adb/box_bll/clash/country.mmdb"
-  rm -rf "/data/adb/box_bll/clash/UpdateGeo.sh"
-  rm -rf "/data/adb/box_bll/clash/ASN.mmdb"
-  rm -rf "/data/adb/box_bll/clash/Update: 数据库.sh"
-  rm -rf "/data/adb/box_bll/clash/Telegram: 聊天组.sh"
-  rm -rf "/data/adb/box_bll/clash/Gui Meta: 在线面板.sh"
-  rm -rf "/data/adb/box_bll/clash/Gui Yacd: 在线面板.sh"
+unzip -qo "${ZIPFILE}" -x 'META-INF/*' -d "$MODPATH"
+if [ -d /data/adb/box_bll ]; then
+  if [ -d /data/adb/box_bll/clash ]; then
+    cp /data/adb/box_bll/clash/config.yaml /data/adb/box_bll/clash/config.yaml.bak
+  fi
+  if [ -d /data/adb/box_bll/scripts ]; then
+    cp /data/adb/box_bll/scripts/box.config /data/adb/box_bll/scripts/box.config.bak
+  fi
+
+  rm -f "/data/adb/box_bll/clash/Gui Yacd: 获取面板.sh"
+  rm -f "/data/adb/box_bll/clash/Gui Meta: 获取面板.sh"
+  rm -f "/data/adb/box_bll/clash/Telegram chat.sh"
+  rm -f "/data/adb/box_bll/clash/country.mmdb"
+  rm -f "/data/adb/box_bll/clash/UpdateGeo.sh"
+  rm -f "/data/adb/box_bll/clash/ASN.mmdb"
+  rm -f "/data/adb/box_bll/clash/Update: 数据库.sh"
+  rm -f "/data/adb/box_bll/clash/Telegram: 聊天组.sh"
+  rm -f "/data/adb/box_bll/clash/Gui Meta: 在线面板.sh"
+  rm -f "/data/adb/box_bll/clash/Gui Yacd: 在线面板.sh"
+  rm -rf /data/adb/box_bll/clash/ui
   rm -rf /data/adb/box_bll/clash/dashboard
-  
-  cp -f $MODPATH/box_bll/clash/config.yaml /data/adb/box_bll/clash/
-  cp -f $MODPATH/box_bll/clash/enhanced_config.yaml /data/adb/box_bll/clash/
-  cp -f $MODPATH/box_bll/clash.Toolbox.sh /data/adb/box_bll/clash/
-  cp -f $MODPATH/box_bll/scripts/* /data/adb/box_bll/scripts/
-  rm -rf $MODPATH/box_bll
+
+  cp -f "$MODPATH/box_bll/clash/config.yaml" /data/adb/box_bll/clash/
+  cp -f "$MODPATH/box_bll/clash/enhanced_config.yaml" /data/adb/box_bll/clash/
+  cp -f "$MODPATH/box_bll/clash.Toolbox.sh" /data/adb/box_bll/clash/
+  cp -f "$MODPATH/box_bll/scripts/"* /data/adb/box_bll/scripts/
+  rm -rf "$MODPATH/box_bll"
 
   ui_print "- Updating..."
   ui_print "- ————————————————"
@@ -50,44 +56,48 @@ if [ -d /data/adb/box_bll ] ; then
   ui_print "- 用户配置 box.config 已备份 bak："
   ui_print "- 可自行选择重新配置或使用默认！"
   ui_print "- ————————————————"
-  ui_print "- 正在等待重启中..."
+  ui_print "- 更新无需重启..."
 else
-  mv $MODPATH/box_bll /data/adb/
+  mv "$MODPATH/box_bll" /data/adb/
   ui_print "- Installing..."
   ui_print "- ————————————————"
   ui_print "- 安装完成 工作目录"
   ui_print "- data/adb/box_bll/"
   ui_print "- ————————————————"
-  ui_print "- 正在等待重启中..."
+  ui_print "- 安装无需重启..."
 fi
 
-if [ "$KSU" = true ] ; then
-  sed -i 's/name=Surfingmagisk/name=SurfingKernelSU/g' $MODPATH/module.prop
+if [ "$KSU" = true ]; then
+  sed -i 's/name=Surfingmagisk/name=SurfingKernelSU/g' "$MODPATH/module.prop"
 fi
 
-if [ "$APATCH" = true ] ; then
-  sed -i 's/name=Surfingmagisk/name=SurfingAPatch/g' $MODPATH/module.prop
+if [ "$APATCH" = true ]; then
+  sed -i 's/name=Surfingmagisk/name=SurfingAPatch/g' "$MODPATH/module.prop"
 fi
 
 mkdir -p /data/adb/box_bll/bin/
 mkdir -p /data/adb/box_bll/run/
 
-mv -f $MODPATH/Surfing_service.sh $service_dir/
+
 
 rm -f customize.sh
 
-set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm_recursive /data/adb/box_bll/ 0 0 0755 0644
-set_perm_recursive /data/adb/box_bll/clash/log/ 0 0 0777 0666
-set_perm_recursive /data/adb/box_bll/run/ 0 0 0755 0666
-set_perm_recursive /data/adb/box_bll/clash/proxy_providers/ 0 0 0755 0666
-set_perm_recursive /data/adb/box_bll/clash/rule/ 0 0 0755 0666
-set_perm_recursive /data/adb/box_bll/scripts/ 0 0 0755 0700
-set_perm_recursive /data/adb/box_bll/bin/ 0 0 0755 0700
+mv -f "$MODPATH/Surfing_service.sh" "$service_dir/"
 
-set_perm $service_dir/Surfing_service.sh 0 0 0700
+set_perm_recursive "$MODPATH" 0 0 0755 0644
+set_perm_recursive /data/adb/box_bll/ 0 3005 0755 0644
+set_perm_recursive /data/adb/box_bll/clash/log/ 0 3005 0777 0666
+set_perm_recursive /data/adb/box_bll/run/ 0 3005 0755 0666
+set_perm_recursive /data/adb/box_bll/clash/proxy_providers/ 0 3005 0755 0666
+set_perm_recursive /data/adb/box_bll/clash/rule/ 0 3005 0755 0666
+set_perm_recursive /data/adb/box_bll/scripts/ 0 3005 0755 0700
+set_perm_recursive /data/adb/box_bll/bin/ 0 3005 0755 0700
 
-chmod ugo+x /data/adb/box_bll/scripts/*
+set_perm "$service_dir/Surfing_service.sh" 0 0 0700
+
+if find /data/adb/box_bll/scripts/ -type f | read; then
+  chmod ugo+x /data/adb/box_bll/scripts/*
+fi
 
 for pid in $(pidof inotifyd) ; do
   if grep -q box.inotify /proc/${pid}/cmdline ; then
@@ -95,4 +105,6 @@ for pid in $(pidof inotifyd) ; do
   fi
 done
 
-inotifyd "/data/adb/box_ll/scripts/box.inotify" "$MODPATH" > /dev/null 2>&1 &
+inotifyd "/data/adb/box_bll/scripts/box.inotify" "$MODPATH" > /dev/null 2>&1 &
+
+/system/bin/sh "$service_dir/Surfing_service.sh" 
