e8afcbbf970f691a24d0408bc019b4fd8c77d089
MoGuangYu <staryuboy@gmail.com>
2024-08-08 22:31:33 +0800

    Update：Uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/system/bin/sh
 
-rm -f "/data/adb/service.d/Surfing_service.sh"
-rm -rf /data/adb/box_bll
-# 2>/dev/null
+rm -f "/data/adb/service.d/Surfing_service.sh" 2>/dev/null
+rm -rf /data/adb/box_bll 2>/dev/null
