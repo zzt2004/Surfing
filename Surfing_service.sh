178b5dd29d8f213125e8d7363528018a001b9bfa
MoGuangYu <staryuboy@gmail.com>
2024-08-10 14:21:50 +0800

    Update：Surfing_service.sh

--- Surfing_service.sh
+++ Surfing_service.sh
@@ -14,3 +14,10 @@ ${scripts_dir}/start.sh
 )&
 
 inotifyd ${scripts_dir}/box.inotify ${module_dir} > /dev/null 2>&1 &
+
+while [ ! -f /data/misc/net/rt_tables ] ; do
+  sleep 3
+done
+
+net_dir="/data/misc/net"
+inotifyd ${scripts_dir}/net.inotify ${net_dir} > /dev/null 2>&1 &
