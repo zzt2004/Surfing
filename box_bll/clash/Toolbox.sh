3ad1f2d4b79bd00b22d971c7a234ac48db2190eb
MoGuangYu <staryuboy@gmail.com>
2025-02-14 01:52:46 +0800

    update Toolbox.sh

--- box_bll/clash/Toolbox.sh
+++ box_bll/clash/Toolbox.sh
@@ -47,7 +47,7 @@ GIT_URL="https://api.github.com/repos/MoGuangYu/Surfing/releases/latest"
 RULES_URL_PREFIX="https://raw.githubusercontent.com/MoGuangYu/rules/main/Home/"
 RULES=("YouTube.yaml" "TikTok.yaml" "Telegram.yaml" "OpenAI.yaml" "Netflix.yaml" "Microsoft.yaml" "Google.yaml" "Facebook.yaml" "Discord.yaml" "Apple.yaml")
 
-CURRENT_VERSION="v11.9"
+CURRENT_VERSION="v12"
 TOOLBOX_URL="https://raw.githubusercontent.com/MoGuangYu/Surfing/main/box_bll/clash/Toolbox.sh"
 TOOLBOX_FILE="/data/adb/box_bll/clash/Toolbox.sh"
 
@@ -507,7 +507,7 @@ else
     fi
 fi
 echo ""
-echo "检测已结束..."
+echo "检测已完毕..."
 
 show_menu() {
     while true; do
